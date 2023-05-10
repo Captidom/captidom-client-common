cf35bee0972896a2da19bf7a70cc7b0eb7293c13
Sebastian Irimia <aisebastian@yahoo.com>
2023-05-10 22:11:26 +0300

    Implemented initial test case for client handling WakeupDescribeMessage

--- test/client/client-test.cpp
+++ test/client/client-test.cpp
@@ -1,8 +1,16 @@
 #include "captidom-client-common/client/client.h"
+#include "captidom-client-common/proto-v1/wakeup-message.h"
 #include "gtest/gtest.h"
 
-TEST(someTest,testOne)
+using namespace captidom;
+
+TEST(client, onWakeupMessageBroadcast)
 {
-  ASSERT_EQ(5,5);
-  ASSERT_EQ(5,5);
+    Client *client = new Client();
+    WakeupMessage *message = (WakeupMessage *)malloc(sizeof(WakeupMessage));
+    client->onMessageReceived(nullptr, &message);
+    ASSERT_STREQ(message->getPlatform(), "platform1");
+    ASSERT_STREQ(message->getIp(), "127.0.0.1");
+    ASSERT_STREQ(message->getVersion(), "0.0.0");
+    free(message);
 }
